af217d04414d56a2a2bbbc8b6bc9c336c36755b5
sofivanhanen <sofia.vanhanen0@gmail.com>
2019-10-14 09:51:20 +0300

    some robot code

--- src/robots/Robots.java
+++ src/robots/Robots.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package robots;
-
-/**
- *
- * @author sofvanh
- */
+import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
 public class Robots {
-
-    /**
-     * @param args the command line arguments
-     */
+    static DifferentialPilot pilot = new DifferentialPilot(5.6, 17.5f, Motor.A, Motor.B);
+    static UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
+    static LightSensor light = new LightSensor(SensorPort.S1);
     public static void main(String[] args) {
-        // TODO code application logic here
+        basic();
     }
     
+    private static void basic() {
+        System.out.println("hello world");
+        Button.waitForAnyPress();
+    }
 }
