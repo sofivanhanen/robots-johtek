869aa5a3c7ebb223c077d6dd3b5a5aae54dacc9c
sofivanhanen <sofia.vanhanen0@gmail.com>
2019-10-14 10:26:05 +0300

    Line following

--- src/robots/Robots.java
+++ src/robots/Robots.java
@@ -6,11 +6,42 @@ public class Robots {
     static UltrasonicSensor ultra = new UltrasonicSensor(SensorPort.S2);
     static LightSensor light = new LightSensor(SensorPort.S1);
     public static void main(String[] args) {
-        basic();
+        simple();
+        followLine();
     }
     
-    private static void basic() {
+    private static void simple() {
         System.out.println("hello world");
         Button.waitForAnyPress();
+    }
+    
+    private static int readLightValue(String location) {
+        System.out.println("press to read " + location);
+        Button.waitForAnyPress();
+        int light = light.readValue();
+        System.out.println("light value of " + location + ": " + light);
+        Button.waitForAnyPress();
+        return light;
+    }
+    
+    private static void followLine() {
+        int floorValue = readLightValue("floor");
+        int tapeValue = readLightValue("tape");
+        System.out.println("Tracking line until infinity");
+        while(true) {
+            int currentValue = light.readValue();
+            if (onTape(currentValue, floorValue, tapeValue)) {
+                pilot.steer(30);
+                waitComplete();
+            }
+            else {
+                pilot.steer(-30);
+                waitComplete();
+            }
+        }
+    }
+    
+    private static boolean onTape(int value, int floor, int tape) {
+        return Math.abs(floor - value) > Math.abs(tape - value);
     }
 }
